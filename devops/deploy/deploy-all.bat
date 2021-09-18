aded1511ca6921144fa65fb82c2efbe40fa7dcdc
Gennady Verdel <godrose@gmail.com>
2021-09-18 13:23:51 +0300

    chore: minor

--- devops/deploy/deploy-all.bat
+++ devops/deploy/deploy-all.bat
@@ -3,4 +3,10 @@ SET version=2.2.2
 rem TODO: Refactor using loop and automatic discovery
 call deploy-single.bat LogoFX.Bootstrapping %version% 
 call deploy-single.bat LogoFX.Server.Bootstrapping %version%
-call deploy-single.bat LogoFX.Client.Core %version%
+call deploy-single.bat LogoFX.Client.Bootstrapping.Adapters.Contracts %version%
+call deploy-single.bat LogoFX.Client.Bootstrapping.Adapters.SimpleContainer %version%
+call deploy-single.bat LogoFX.Client.Bootstrapping.Adapters.Unity %version%
+call deploy-single.bat LogoFX.Client.Bootstrapping.Core %version%
+call deploy-single.bat LogoFX.Client.Bootstrapping %version%
+call deploy-single.bat LogoFX.Client.Bootstrapping.Testing %version%
+call deploy-single.bat LogoFX.Client.Bootstrapping.Xamarin.Forms %version%
