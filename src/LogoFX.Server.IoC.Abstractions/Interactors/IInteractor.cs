ac22701e09cc7c515675de19d53d0e32cfe5d6e0
Gennady Verdel <godrose@gmail.com>
2022-01-02 11:36:04 +0200

    feat(server-abstractions): added common abstractions #13

---
+++ src/LogoFX.Server.IoC.Abstractions/Interactors/IInteractor.cs
@@ -0,0 +1,10 @@
+﻿namespace LogoFX.Server.IoC.Abstractions.Interactors
+{
+    /// <summary>
+    /// This is a marker interface.
+    /// Implement to register the interactor automatically.
+    /// </summary>
+    public interface IInteractor
+    {
+    }
+}
