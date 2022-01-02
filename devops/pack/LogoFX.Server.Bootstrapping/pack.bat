422e007fffd9d0398eb477eaffefaac58dada723
Gennady Verdel <godrose@gmail.com>
2022-01-02 11:55:10 +0200

    feat(server-abstractions): added common abstractions #13 (#14)

--- devops/pack/LogoFX.Server.Bootstrapping/pack.bat
+++ devops/pack/LogoFX.Server.Bootstrapping/pack.bat
@@ -3,6 +3,6 @@ rmdir /Q /S lib
 mkdir lib
 cd lib
 mkdir netstandard2.0\
-robocopy ../../../../../src/Bin/netstandard/Release netstandard2.0 LogoFX.Server.Bootstrapping.* /E
+robocopy ../../../../../src/Bin/netstandard/Release netstandard2.0 LogoFX.Server.* /E
 cd ../../
 nuget pack contents/LogoFX.Server.Bootstrapping.nuspec -OutputDirectory ../../../output
