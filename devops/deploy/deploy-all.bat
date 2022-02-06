69cfa4221d78e9e90e66beb1e97ef7ffd5ac8185
Gennady Verdel <godrose@gmail.com>
2022-02-06 13:07:30 +0200

    feat(registrration): added logofx.server.ioc.abstractions package

--- devops/deploy/deploy-all.bat
+++ devops/deploy/deploy-all.bat
@@ -2,6 +2,7 @@ rem TODO: Use common source for all version instances
 SET version=2.2.6-rc1
 rem TODO: Refactor using loop and automatic discovery
 call deploy-single.bat LogoFX.Bootstrapping %version% 
+call deploy-single.bat LogoFX.Server.IoC.Abstractions %version%
 call deploy-single.bat LogoFX.Server.Bootstrapping %version%
 call deploy-single.bat LogoFX.Client.Bootstrapping.Adapters.Contracts %version%
 call deploy-single.bat LogoFX.Client.Bootstrapping.Adapters.SimpleContainer %version%
--- devops/pack/LogoFX.Server.Bootstrapping/pack.bat
+++ devops/pack/LogoFX.Server.Bootstrapping/pack.bat
@@ -3,8 +3,11 @@ rmdir /Q /S lib
 mkdir lib
 cd lib
 mkdir net6.0
-robocopy ../../../../../src/Bin/net/Release net6.0 LogoFX.Server.*.dll /E
-robocopy ../../../../../src/Bin/net/Release net6.0 LogoFX.Server.*.json /E
-robocopy ../../../../../src/Bin/net/Release net6.0 LogoFX.Server.*.xml /E
+robocopy ../../../../../src/Bin/net/Release net6.0 LogoFX.Server.Bootstrapping.dll /E
+robocopy ../../../../../src/Bin/net/Release net6.0 LogoFX.Server.Bootstrapping.json /E
+robocopy ../../../../../src/Bin/net/Release net6.0 LogoFX.Server.Bootstrapping.xml /E
+robocopy ../../../../../src/Bin/net/Release net6.0 LogoFX.Server.IoC.Registration.dll /E
+robocopy ../../../../../src/Bin/net/Release net6.0 LogoFX.Server.IoC.Registration.json /E
+robocopy ../../../../../src/Bin/net/Release net6.0 LogoFX.Server.IoC.Registration.xml /E
 cd ../../
 nuget pack contents/LogoFX.Server.Bootstrapping.nuspec -OutputDirectory ../../../output
