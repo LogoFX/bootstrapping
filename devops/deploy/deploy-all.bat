5bddc600cff17514b708a499d7e566c09904ecd4
Gennady Verdel <godrose@gmail.com>
2022-10-21 11:34:33 +0300

    chore: bump version to 2.2.7

--- devops/deploy/deploy-all.bat
+++ devops/deploy/deploy-all.bat
@@ -1,5 +1,5 @@
 rem TODO: Use common source for all version instances
-SET version=2.2.7-rc1
+SET version=2.2.7
 rem TODO: Refactor using loop and automatic discovery
 call deploy-single.bat LogoFX.Bootstrapping %version% 
 call deploy-single.bat LogoFX.Server.IoC.Abstractions %version%
